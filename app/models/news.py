f7c444e45e7401e2dfa01a3ea4d93e02108381ad
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-05-01 10:14:38 +0300

    make an api call

--- app/models/news.py
+++ app/models/news.py
@@ -1,31 +1,7 @@
 
-class Article:
+class News:
     '''
-    Class that instantiates objects of the news article objects of the news sources
-    '''
-    def __init__(self,author,description,time,url,image,title):
-        self.author = author
-        self.description = description
-        self.time = time
-        self.url = url
-        self.image = image
-        self.title = title
-
-class Category:
-    '''
-    Class that instantiates objects of the news categories objects of the news sources
-    '''
-    def __init__(self,author,description,time,url,image,title):
-        self.author = author
-        self.description = description
-        self.time = time
-        self.url = url
-        self.image = image
-        self.title = title
-
-class Source:
-    '''
-    Source class to define source objects
+    News class to define news objects
     '''
     def __init__(self,id,name,description,url):
         self.id = id
@@ -33,14 +9,5 @@ class Source:
         self.description = description
         self.url = url
 
-class Headlines:
-    '''
-    Class that instantiates objects of the headlines categories objects of the news sources
-    '''
-    def __init__(self,author,description,time,url,image,title):
-        self.author = author
-        self.description = description
-        self.time = time
-        self.url = url
-        self.image = image
-        self.title = title
+    
+
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 # Views
 @app.route('/')
@@ -8,4 +9,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    popular_news = get_news('popular')
+    print(popular_news)
+    title = 'Home-Welcome to the best news channel'
+    return render_template('index.html',title = title,popular=popular_news)
