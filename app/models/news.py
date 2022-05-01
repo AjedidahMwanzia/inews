f691cba6919f2f7e4088c528927caef37f708589
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-05-01 11:34:18 +0300

    update models file

--- app/models/news.py
+++ app/models/news.py
@@ -1,7 +1,30 @@
-
-class News:
+class Article:
     '''
-    News class to define news objects
+    Class that instantiates objects of the news article objects of the news sources
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
+
+class Category:
+    '''
+    Class that instantiates objects of the news categories objects of the news sources
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
+
+class Source:
+    '''
+    Source class to define source objects
     '''
     def __init__(self,id,name,description,url):
         self.id = id
@@ -9,5 +32,14 @@ class News:
         self.description = description
         self.url = url
 
-    
-
+class Headlines:
+    '''
+    Class that instantiates objects of the headlines categories objects of the news sources
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
