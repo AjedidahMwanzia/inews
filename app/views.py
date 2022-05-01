8e57af16b3a890cd7e297f02f504129a173e6c0f
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-05-01 10:24:11 +0300

    pass news to the template

--- app/views.py
+++ app/views.py
@@ -10,6 +10,6 @@ def index():
     View root page function that returns the index page and its data
     '''
     popular_news = get_news('popular')
-    print(popular_news)
+    now_live_news = get_news('now_live')
     title = 'Home-Welcome to the best news channel'
-    return render_template('index.html',title = title,popular=popular_news)
+    return render_template('index.html',title = title,popular=popular_news,now_live=now_live_news)
